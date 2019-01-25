1e754e06a93a15b6f7708b6c1ad6b577299dc022
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-01-25 17:56:06 -0500

    Functioning

---
+++ logic.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+// Variable showImage will hold the setInterval when we start the slideshow
+var showImage;
+const imgDiv = $(".imgDiv");
+let interval;
+
+$("#start").on("click", function(event) {
+  startSlideshow();
+});
+
+$("#stop").on("click", function(event) {
+  stopSlideshow();
+});
+
+// Links
+// https://media.giphy.com/media/11FuEnXyGsXFba/giphy.gif
+// https://source.unsplash.com/random
+
+const createPic = () => {
+interval = setInterval(function() {
+  imgDiv.empty();
+  let pic = $("<img>").attr("src","https://source.unsplash.com/random").addClass("img-fluid");
+  imgDiv.append(pic);
+}, 1000 * 15)
+  
+};
+
+// function displayImage() {
+//   $("#imgPlace").attr("src", "https://media.giphy.com/media/11FuEnXyGsXFba/giphy.gif");
+// };
+
+// function nextImage() {
+
+// $("#imgPlace").attr("src", "https://source.unsplash.com/random");
+// console.log("Next Picture!");
+// setTimeout(displayImage, 1000);
+// };
+
+// function startSlideshow() {
+
+// showImage = setInterval(nextImage, 1000 * 60);
+
+// };
+
+function stopSlideshow() {
+
+clearInterval(interval);
+};
+
+// This will run the display image function as soon as the page loads.
+// displayImage();
+
+createPic();
+});
